da3b885f0e5855b189094c5643121c92e2de47a0
stuber <tod.p.stuber@usda.gov>
2017-02-23 18:51:22 -0600

    catch AC1 defining SNPs when chrom > 1

--- bin/vcftofasta.sh
+++ bin/vcftofasta.sh
@@ -1140,12 +1140,18 @@ echo "AConeCallPosition is running, started -->  `date`"
 #echo "Possible Mixed Isolates" > section2
 #echo "Defining SNPs that are called as AC=1" >> section2
 echo "" >> section2
-
-for i in *.vcf; do
-    (for pos in $positionList; do group_id=`grep "$pos" ${DefiningSNPs} | awk '{print $4 "-" $1}'`; awk -v x=$pos -v g=$group_id 'BEGIN {FS="\t"; OFS="\t"} { if($2 == x ) print FILENAME, g, "Pos:", $2, "QUAL:", $6, $8 }' $i 2> /dev/null; done | grep "AC=1;A" | awk 'BEGIN {FS=";"} {print $1, $2}' >> section2) & 
-    let count+=1
-    [[ $((count%NR_CPUS)) -eq 0 ]] && wait
-done
+if [ $((chromCount)) -eq 1 ]; then
+    for i in *.vcf; do
+        (for pos in $positionList; do group_id=`grep "$pos" ${DefiningSNPs} | awk '{print $4 "-" $1}'`; awk -v x=$pos -v g=$group_id 'BEGIN {FS="\t"; OFS="\t"} { if($2 == x ) print FILENAME, g, "Pos:", $2, "QUAL:", $6, $8 }' $i 2> /dev/null; done | grep "AC=1;A" | awk 'BEGIN {FS=";"} {print $1, $2}' >> section2) &
+        let count+=1
+        [[ $((count%NR_CPUS)) -eq 0 ]] && wait
+    done
+else
+    for i in *.vcf; do
+        (for pos in $positionList; do group_id=`grep "$pos" ${DefiningSNPs} | awk '{print $4 "-" $1}'`; awk -v x=$pos -v g=$group_id 'BEGIN {FS="\t"; OFS="\t"} { if($1 "-" $2 == x ) print FILENAME, g, "Pos:", $2, "QUAL:", $6, $8 }' $i 2> /dev/null; done | grep "AC=1;A" | awk 'BEGIN {FS=";"} {print $1, $2}' >> section2) &
+        let count+=1
+        [[ $((count%NR_CPUS)) -eq 0 ]] && wait
+fi
 
 echo "AConeCallPosition is running, end -->  `date`"
 wait

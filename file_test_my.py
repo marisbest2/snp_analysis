3fca62508dcd4d2ed1804e66f2b709615869bbf0
stuber <tod.p.stuber@usda.gov>
2017-07-24 18:21:39 -0500

    wip

--- file_test_my.py
+++ file_test_my.py
@@ -2,12 +2,37 @@
 
 import glob
 
-R1 = glob.glob('*_R1*fastq.gz')
-R2 = glob.glob('*_R2*fastq.gz')
-list_len = len(list_of_files)
-if (list_len % 2 != 0):
-    print("\n#####Check paired files.  Unpaired files seen by odd number of counted FASTQs\n\n")
+all_file_types_count = len(glob.glob('*.*'))
+fastq_check = len(glob.glob('*fastq.gz'))
+vcf_check = len(glob.glob('*vcf'))
+
+if fastq_check > 0:
+    fastq_check = True
+if vcf_check > 0:
+    vcf_check = True
+if fastq_check and vcf_check:
+    print("\n#####You have a mix of FASTQ and VCF files.  This is not allowed\n\n")
     sys.exit(0)
+
+if fastq_check:
+    R1 = glob.glob('*_R1*fastq.gz')
+    R2 = glob.glob('*_R2*fastq.gz')
+    R1count = len(R1)
+    R2count = len(R2)
+    fastq_count = R1count + R2count
+    if (fastq_count % 2 != 0):
+        print("\n#####Check paired files.  Unpaired files seen by odd number of counted FASTQs\n\n")
+        sys.exit(0)
+    if (R1count != R2count):
+        print("\n#####Check paired files.  R1 files do not equal R2\n\n")
+        sys.exit(0)
+    all_file_types_count = len(glob.glob('*.*'))
+    if (all_file_types_count != fastq_count):
+        print("\n#####Only zipped FASTQ files are allowed in directory\n\n")
+        sys.exit(0)
+    elif (fastq_count > 1):
+        execute script 1
+
 
 
 

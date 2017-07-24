16ead3ccaf9b493395160826c483a30479b18411
stuber <tod.p.stuber@usda.gov>
2017-07-24 18:26:45 -0500

    wip

--- file_test_my.py
+++ file_test_my.py
@@ -26,14 +26,18 @@ if fastq_check:
     if (R1count != R2count):
         print("\n#####Check paired files.  R1 files do not equal R2\n\n")
         sys.exit(0)
-    all_file_types_count = len(glob.glob('*.*'))
     if (all_file_types_count != fastq_count):
         print("\n#####Only zipped FASTQ files are allowed in directory\n\n")
         sys.exit(0)
     elif (fastq_count > 1):
         execute script 1
+elif vcf_check:
+    vcfs_count = len(glob.glob('*vcf'))
+    if (all_file_types_count != vcfs_count):
+        print("\n#####You have more than just VCF files in your directory.  Only VCF files are allowed if running script 2\n\n")
+        sys.exit(0)
+else:
+    (print "######There was an error determining the file type.")
+        sys.exit(0)
 
 
-
-
-fastqs = glob.glob(zips + '/*.vcf')

950d7776b3b80cd8622851bbe52f8d5d55030566
stuber <tod.p.stuber@usda.gov>
2017-07-26 15:06:41 -0500

    cross reference started

--- reference_species_finder.py
+++ reference_species_finder.py
@@ -1,2 +1,6 @@
 #!/usr/bin/env python
 
+#species = corresponding NCBI accession
+species_cross_reference = {}
+species_cross_reference["bovis"] = ["002945", "00879"]
+species_cross_reference["h37"] = ["000962", "002755", "009525", "018143"]

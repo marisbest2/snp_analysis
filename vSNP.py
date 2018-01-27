e6019e998001251c145cb73370872c4068568759
stuber <tstuber@AAPIAAM2V40111D.local>
2018-01-27 11:55:15 -0600

    wip

--- vSNP.py
+++ vSNP.py
@@ -2453,6 +2453,7 @@ class script2():
                     except IndexError:
                         #print ("except IndexError: when changing names")
                         elite_test = ""
+                    print("newname %s" % newname)
                     if new_name[-1] != "_":
                         new_name = new_name + "_"
                     code_dictionary.update({new_name:elite_test})
